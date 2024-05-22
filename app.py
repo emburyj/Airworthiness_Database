93e7c3046310fc4718cdb15137f37b9d32cdedf4
Josh <joshembury@gmail.com>
2024-05-22 17:46:07 -0600

    Removed hard-coded example data

--- app.py
+++ app.py
@@ -29,54 +29,24 @@ def about():
 def Airworthiness_Directives():
     entities = ["AD ID", "Airworthiness Directive Number",
                  "Airworthiness Directive Description", "Maintenance Required Date"]
-    desc = [
-            """The loss of the SPCU and ground through the P6 panel could result
-            in the loss of significant flightcrew instrumentation and displays.
-            This AD requires installing two bonding jumpers from the P6 panel
-            structure to primary structure.""",
-            """This emergency AD was prompted by a report of an in-flight departure
-                of a mid cabin door plug, which resulted in a rapid decompression of
-                the airplane. The FAA is issuing this AD to address the potential
-                in-flight loss of a mid cabin door plug, which could result in injury
-                to passengers and crew, the door impacting the airplane, and/or loss
-                of control of the airplane.""",
-                """To address incomplete installations of the over wing panel lug attachments
-                in the production assembly line, which, if not detected and corrected, could
-                reduce the structural integrity of the wing."""
-            ]
 
-    data = [
-            {"ID": 1, "Number": "2024-06-03", "Description": desc[0], "Date": "2024-05-02"},
-            {"ID": 2, "Number": "2024-02-51_Emergency", "Description": desc[1], "Date": "2024-01-06"},
-            {"ID": 3, "Number": "2024-06-07", "Description": desc[2], "Date": "2024-05-22"},
-    ]
+    data = ()
 
     return render_template("Airworthiness_Directives.html", entities=entities, data=data, page_name="Airworthiness Directives")
 
 @app.route('/Registered_Aircraft')
 def Registered_Aircraft():
     entities = ["Aircraft ID", "N Number", "Owner ID","Owner Name", "Model ID","Model Name", "Current Status"]
-    data = {
-            "Owners": ["Josh Embury", "Ian Bubier", "United Airlines", "Alaska Airlines"],
-            "Models": ["737-8", "737-9", "A320-214", "E1000"],
-            "Aircraft": [
-                        {"ID": 1, "Number": "N921AK", "OID": 1,"Owner": "Josh Embury", "MID": 2, "Model": "737-9", "Status": "Grounded for Maintenance"},
-                        {"ID": 2, "Number": "N200MR", "OID": 2,"Owner": "Ian Bubier", "MID": 4, "Model": "E1000", "Status": "In-Service"},
-                        {"ID": 3, "Number": "N291BT", "OID": 4,"Owner": "Alaska Airlines", "MID": 2, "Model": "737-9", "Status": "Pending Maintenance"},
-                        ]
-            }
+    data = ()
+
     return render_template("Registered_Aircraft.html",entities=entities, data=data, page_name="Registered Aircraft")
 
 @app.route('/Aircraft_Models')
 def Aircraft_Models():
     entities = ["Model ID", "Manufacturer Name", "Model Name"]
-    model_data = [
-                    {"ID": 1, "Manufacturer": "The Boeing Company", "Model": "737-8"},
-                    {"ID": 2, "Manufacturer": "The Boeing Company", "Model": "737-9"},
-                    {"ID": 3, "Manufacturer": "Airbus SAS", "Model": "A320-214"},
-                    {"ID": 4, "Manufacturer": "Epic Aircraft", "Model": "E1000"}
-                ]
-    return render_template("Aircraft_Models.html", entities=entities, data=model_data, page_name="Aircraft Models")
+    data = ()
+
+    return render_template("Aircraft_Models.html", entities=entities, data=data, page_name="Aircraft Models")
 
 @app.route('/Aircraft_Owners', methods=["POST", "GET"])
 def Aircraft_Owners():
@@ -134,13 +104,7 @@ def Aircraft_Owners():
 def Maintenance_Records():
     entities = ["Maintenance ID", "Aircraft ID", "N Number", "Maintenance Date", "Maintenance Description"]
 
-    data = [
-            {"MID": 1, "AID": 1, "Number": "N921AK", "Date": "2018-10-30", "Description": "Fixed a thing."},
-            {"MID": 2, "AID": 1, "Number": "N921AK", "Date": "2019-01-15", "Description": "Repaired a doodad."},
-            {"MID": 3, "AID": 1, "Number": "N921AK", "Date": "2021-06-18", "Description": "Mended a thingamajig."},
-            {"MID": 4, "AID": 2, "Number": "N200MR", "Date": "2023-07-29", "Description": "Reconfigured a whatsit."},
-            {"MID": 5, "AID": 3, "Number": "N291BT", "Date": "2024-01-05", "Description": "Jury-rigged a whatchamacallit."}
-            ]
+    data = []
 
     return render_template("Maintenance_Records.html", entities=entities, data=data, page_name="Maintenance Records")
 
@@ -148,12 +112,7 @@ def Maintenance_Records():
 def Models_Directives():
     entities = ["Models Directives ID", "Model ID", "Model Name", "AD ID", "Airworthiness Directive Number"]
 
-    data = [
-            {"MDID": 1, "MID": 2, "Model": "737-9", "ADID": 2, "Number": "2024-02-51_Emergency"},
-            {"MDID": 2, "MID": 1, "Model": "737-8", "ADID": 1, "Number": "2024-06-03"},
-            {"MDID": 3, "MID": 2, "Model": "737-9", "ADID": 1, "Number": "2024-06-03"},
-            {"MDID": 4, "MID": 3, "Model": "A320-214", "ADID": 3, "Number": "2024-06-07"}
-            ]
+    data = []
 
     return render_template("Models_Directives.html", entities=entities, data=data, page_name="Models Impacted by Directives")
 
